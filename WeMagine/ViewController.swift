9b8d96f3abdbe20a8661e0bcf2c914b9103544f1
Victor <victoraugustolls@gmail.com>
2015-03-13 10:33:31 -0300

    Menu Lateral e ajustes na votação

--- WeMagine/ViewController.swift
+++ WeMagine/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     var sizeRect = UIScreen.mainScreen().applicationFrame;
     let app = UIApplication.sharedApplication()
-    var tap: UITapGestureRecognizer = UITapGestureRecognizer()
+    var menuView:UIView = UIView()
+    var badIdea: UIView = UIView()
+    var goodIdea: UIView = UIView()
+    var menuViewHidden: Bool = true
+    var mayVote: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +29,6 @@ class ViewController: UIViewController {
         var y:CGFloat
         
         var barHeight:CGFloat = app.statusBarFrame.size.height
-        
-//        Blue & Red rectangles
-        
-        var badIdea: UIView = UIView(frame: CGRectMake(0, 0, sizeRect.size.width/2, sizeRect.size.height+barHeight))
-        badIdea.backgroundColor = UIColor(red: 0.9995, green: 0.4959, blue: 0.562, alpha: 1.0)
-        badIdea.userInteractionEnabled = true
-        self.view.addSubview(badIdea)
-        
-        var goodIdea: UIView = UIView(frame: CGRectMake(sizeRect.size.width/2, 0, sizeRect.size.width/2, sizeRect.size.height+barHeight))
-        goodIdea.backgroundColor = UIColor(red: 0.6192, green: 0.9166, blue: 1.0, alpha: 1.0)
-        goodIdea.userInteractionEnabled = true
-        self.view.addSubview(goodIdea)
         
 //        Top rectangle
         
@@ -55,9 +47,15 @@ class ViewController: UIViewController {
         y = ((59.0 * prop) - height)/2 + barHeight
         
         var menuOpen:UIImage = UIImage(named: "MenuIcon@3x.png")!
-        var menuOpenIcon:UIImageView = UIImageView(image: menuOpen)
+        var menuOpenIcon: UIImageView = UIImageView(image: menuOpen)
         menuOpenIcon.frame = CGRectMake(x, y, width, height)
+        menuOpenIcon.userInteractionEnabled = true
+        menuOpenIcon.tag = 1
         self.view.addSubview(menuOpenIcon)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action:Selector("handleTap:"))
+        recognizer.delegate = self
+        menuOpenIcon.addGestureRecognizer(recognizer)
         
         width = 38.0 * prop
         height = 35.0 * prop
@@ -68,6 +66,26 @@ class ViewController: UIViewController {
         var lampIconView:UIImageView = UIImageView(image: lampIcon)
         lampIconView.frame = CGRectMake(x, y, width, height)
         self.view.addSubview(lampIconView)
+        
+        //        Blue & Red rectangles
+        
+        badIdea = UIView(frame: CGRectMake(0, (59.0 * prop) + barHeight, sizeRect.size.width/2, sizeRect.size.height+barHeight))
+        badIdea.backgroundColor = UIColor(red: 0.9995, green: 0.4959, blue: 0.562, alpha: 1.0)
+        badIdea.userInteractionEnabled = true
+        self.view.addSubview(badIdea)
+        
+        let badVote = UITapGestureRecognizer(target: self, action:Selector("badVote:"))
+        badVote.delegate = self
+        badIdea.addGestureRecognizer(badVote)
+        
+        goodIdea = UIView(frame: CGRectMake(sizeRect.size.width/2, (59.0 * prop) + barHeight, sizeRect.size.width/2, sizeRect.size.height+barHeight))
+        goodIdea.backgroundColor = UIColor(red: 0.6192, green: 0.9166, blue: 1.0, alpha: 1.0)
+        goodIdea.userInteractionEnabled = true
+        self.view.addSubview(goodIdea)
+        
+        let goodVote = UITapGestureRecognizer(target: self, action:Selector("goodVote:"))
+        goodVote.delegate = self
+        goodIdea.addGestureRecognizer(goodVote)
         
 //        Cloud
         
@@ -114,6 +132,29 @@ class ViewController: UIViewController {
         textView.textAlignment = .Center
 //        textView.backgroundColor = UIColor.redColor()
         cloudImageView.addSubview(textView)
+        
+//        Menu View
+        
+        menuView = UIView(frame: CGRect(x: -sizeRect.size.width/2, y: topMenuRectangle.frame.size.height, width: sizeRect.size.width/2, height: sizeRect.size.height))
+        menuView.backgroundColor = UIColor(red: 0.9601, green: 0.9307, blue: 0.9053, alpha: 1.0)
+        self.view.addSubview(menuView)
+        
+//        Menu View Rectangles
+        
+        var share:UIView = UIView(frame: CGRect(x: 0, y: 0, width: sizeRect.size.width/2, height: sizeRect.size.height/10))
+        share.backgroundColor = UIColor(red: 0.7357, green: 0.639, blue: 0.5589, alpha: 1.0)
+        menuView.addSubview(share)
+        
+        var profile:UIView = UIView(frame: CGRect(x: 0, y: 2*sizeRect.size.height/10, width: sizeRect.size.width/2, height: sizeRect.size.height/10))
+        profile.backgroundColor = UIColor(red: 0.7357, green: 0.639, blue: 0.5589, alpha: 1.0)
+        menuView.addSubview(profile)
+        
+        var logout:UIView = UIView(frame: CGRect(x: 0, y: 4*sizeRect.size.height/10, width: sizeRect.size.width/2,
+            height: sizeRect.size.height/10))
+        logout.backgroundColor = UIColor(red: 0.7357, green: 0.639, blue: 0.5589, alpha: 1.0)
+        menuView.addSubview(logout)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -121,18 +162,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        let touch = touches.allObjects[0] as UITouch
-        let touchLocation = touch.locationInView(self.view)
-        println(touchLocation)
-        
-        if touchLocation.x < sizeRect.width/2
+    func goodVote(recognizer: UITapGestureRecognizer) {
+        if(mayVote)
         {
-            println("esquerda")
+            println("goodVote")
         }
-        else
+    }
+    
+    func badVote(recognizer: UITapGestureRecognizer) {
+        if(mayVote)
         {
-            println("direita")
+            println("badVote")
+        }
+    }
+    
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        
+        if(menuViewHidden){
+            UIView.animateWithDuration(0.5, animations: {
+                self.menuView.frame.origin.x = 0
+                self.menuViewHidden = false
+                self.mayVote = false
+            })
+        }
+        else{
+            UIView.animateWithDuration(0.5, animations: {
+                self.menuView.frame.origin.x = -self.sizeRect.size.width/2
+                self.menuViewHidden = true
+                self.mayVote = true
+            })
         }
         
     }
